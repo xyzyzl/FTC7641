2942183571c8c59d35fa77e367cb28a7f92193c8
Albert Ye <41131127+xyzyzl@users.noreply.github.com>
2018-10-02 16:39:19 -0700

    Update TO.java

--- TO.java
+++ TO.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.*;
 
-@TeleOp
+@TeleOp(name="abinavkapoor")
 public class TO extends LinearOpMode {
 	// mecanum wheels
 	private DcMotor leftFront;
